6c4c2ca7fcf053a8f49053163a41334dc16a4a0b
ruben33206-png <ruben33206@esfundao.pt>
2026-02-18 10:02:59 +0000

    BasedeDados Resolvido

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from database import engine
 from models import Base
+import uvicorn
 
 app = FastAPI()
 
-# Cria as tabelas no banco
 Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
-    return {"message": "API funcionando 🚀"}
+    return {"message": "API funcionando"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
